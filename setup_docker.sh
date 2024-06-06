5aac8e4ee38f5daf3c3dc1f15a7bed515027b995
sealmindset <sealmindset@gmail.com>
2024-06-06 09:11:44 -0500

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -52,6 +52,44 @@ RUN apt-get update && apt-get install -y \
     gnupg1 \
     && apt-get clean
 
+RUN apt-get install build-essential \
+    cmake \
+    git \
+    unzip \
+    pkg-config \
+    libjpeg-dev \
+    libpng-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libswscale-dev \
+    libgtk2.0-dev \
+    libcanberra-gtk* \
+    libgtk-3-dev \
+    libgstreamer1.0-dev \
+    gstreamer1.0-gtk3 \
+    libgstreamer-plugins-base1.0-dev \
+    gstreamer1.0-gl \
+    libxvidcore-dev \
+    libx264-dev \
+    python3-dev \
+    python3-numpy \
+    libtbb2 \
+    libtbb-dev \
+    libdc1394-22-dev \
+    libv4l-dev \
+    v4l-utils \
+    libopenblas-dev \
+    libatlas-base-dev \
+    libblas-dev \
+    liblapack-dev \
+    gfortran \
+    libhdf5-dev \
+    libprotobuf-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    protobuf-compiler \
+    && apt-get clean
+
 RUN echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | \
     tee /etc/apt/sources.list.d/coral-edgetpu.list
 
@@ -63,6 +101,13 @@ RUN pip3 install pycoral
 
 RUN apt-get update
 RUN apt-get install python3-tflite-runtime
+
+# Coral
+RUN mkdir google-coral && cd google-coral
+RUN git clone https://github.com/google-coral/examples-camera --depth 1
+
+RUN pip3 install opencv-contrib-python
+RUN apt-get install python3-opencv
 
 # Create a directory for the Pycoral scripts
 WORKDIR /pycoral
